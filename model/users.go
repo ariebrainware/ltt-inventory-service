395dd501faabd82dcace7c1c74cc2e83b1226ba3
Arie Brainware <publicu001@protonmail.com>
2025-04-03 16:52:28 +0700

    Refactor User model to replace role string with role_id as uint32

--- model/users.go
+++ model/users.go
@@ -9,5 +9,5 @@ type User struct {
 	Name     string `gorm:"type:varchar(100);not null" json:"name"`
 	Email    string `gorm:"type:varchar(100);uniqueIndex;not null" json:"email"`
 	Password string `gorm:"type:varchar(255);not null" json:"-"`
-	Role     string `gorm:"type:varchar(50);not null" json:"role"`
+	RoleID   uint32 `gorm:"type:int(32);not null" json:"role_id"`
 }
